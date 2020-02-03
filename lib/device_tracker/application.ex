b564dc4d67d3b24ce8f5910b512855f5a3aee9b6
Devon Estes <devon.c.estes@gmail.com>
2020-02-03 21:58:31 +0100

    serving seeds on server start

--- lib/device_tracker/application.ex
+++ lib/device_tracker/application.ex
@@ -1,30 +1,41 @@
 defmodule DeviceTracker.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     children = [
-      # Start the endpoint when the application starts
       DeviceTrackerWeb.Endpoint,
       {DynamicSupervisor, strategy: :one_for_one, name: DeviceTracker.DynamicSupervisor},
       {Registry, keys: :unique, name: DeviceTracker.Registry}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: DeviceTracker.Supervisor]
-    Supervisor.start_link(children, opts)
+    return = Supervisor.start_link(children, opts)
+
+    if Mix.env() == :dev, do: seeds()
+
+    return
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     DeviceTrackerWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp seeds() do
+    devices = [
+      {"lightbulb5", ["power_usage"]},
+      {"lightbulb6", ["other_usage"]},
+      {"lightbulb7", ["third_usage"]}
+    ]
+
+    Enum.each(devices, fn {name, measurements} ->
+      DeviceTracker.Devices.Device.add_device(name, measurements)
+
+      Enum.each(measurements, fn measurement ->
+        DeviceTracker.Devices.Device.add_measurement(name, measurement, 123)
+      end)
+    end)
   end
 end
