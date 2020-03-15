15f58860269b4b09158811196763d9d2c7c782e7
Devon Estes <devon.c.estes@gmail.com>
2020-03-15 14:27:52 +0100

    fix name clashes

--- test/device_tracker/devices/device_test.exs
+++ test/device_tracker/devices/device_test.exs
@@ -17,45 +17,39 @@ defmodule DeviceTracker.Devices.DeviceTest do
 
   describe "add_device/2" do
     test "allows us to register a device", %{registry: registry} do
-      name = "lightbulb"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
 
-      assert {:ok, %{measurements: ["power_used"], name: "lightbulb"}} =
+      assert {:ok, %{measurements: [^measurement], name: ^name}} =
                Device.add_device(name, [measurement], S3, registry)
 
-      assert Registry.count(registry) >= 1
-    end
-
-    test "creates an S3 bucket", %{registry: registry} do
-      name = "lightbulb_again"
-      measurement = "power_used"
-      Device.add_device(name, [measurement], S3, registry)
-      assert_receive({:put_bucket, ^name})
+      assert Registry.count(registry) == 1
+      assert_received({:put_bucket, ^name})
     end
   end
 
   describe "add_measurement/3" do
     test "allows us to add measurements", %{registry: registry} do
-      name = "lightbulb2"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
       assert {:ok, _} = Device.add_device(name, [measurement], S3, registry)
 
-      assert {:ok, %{measurement: "power_used", measurements: [1]}} =
+      assert {:ok, %{measurement: ^measurement, measurements: [1]}} =
                Device.add_measurement(name, measurement, 1, S3, registry)
 
-      assert {:ok, %{measurement: "power_used", measurements: [2, 1]}} =
+      assert {:ok, %{measurement: ^measurement, measurements: [2, 1]}} =
                Device.add_measurement(name, measurement, 2, S3, registry)
     end
 
     test "uploads results to S3", %{registry: registry} do
-      name = "lightbulb2_again"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
       Device.add_device(name, [measurement], S3, registry)
       Device.add_measurement(name, measurement, 1, S3, registry)
 
       expected_state = %{
-        measurements: %{power_used: %{measurements: [1]}},
-        name: "lightbulb2_again",
+        measurements: %{String.to_atom(measurement) => %{measurements: [1]}},
+        name: name,
         power_status: :on
       }
 
@@ -66,8 +60,8 @@ defmodule DeviceTracker.Devices.DeviceTest do
 
   describe "get_measurements/2" do
     test "gets measurements for a given device", %{registry: registry} do
-      name = "lightbulb3"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
       assert {:ok, _} = Device.add_device(name, [measurement], S3, registry)
       assert {:ok, []} = Device.get_measurements(name, measurement, registry)
       assert {:ok, _} = Device.add_measurement(name, measurement, 456, S3, registry)
@@ -77,11 +71,19 @@ defmodule DeviceTracker.Devices.DeviceTest do
 
   describe "get/1" do
     test "gets all information for the given device", %{registry: registry} do
-      name = "lightbulb4"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
       assert {:ok, _} = Device.add_device(name, [measurement], S3, registry)
       assert {:ok, _} = Device.add_measurement(name, measurement, 456, S3, registry)
-      assert {:ok, %{}} = Device.get(name, registry)
+
+      measurement = String.to_atom(measurement)
+
+      assert {:ok,
+              %{
+                name: name,
+                measurements: %{measurement => %{measurements: [456]}},
+                power_status: :on
+              }} == Device.get(name, registry)
     end
 
     test "returns an error tuple if the device doesn't exist", %{registry: registry} do
@@ -91,11 +93,7 @@ defmodule DeviceTracker.Devices.DeviceTest do
 
   describe "list_all/0" do
     test "lists all information for all devices", %{registry: registry} do
-      devices = [
-        {"lightbulb5", ["power_usage"]},
-        {"lightbulb6", ["other_usage"]},
-        {"lightbulb7", ["third_usage"]}
-      ]
+      devices = Enum.map(0..2, fn _ -> {random_string(), [random_string()]} end)
 
       Enum.each(devices, fn {name, measurements} ->
         Device.add_device(name, measurements, S3, registry)
@@ -104,17 +102,21 @@ defmodule DeviceTracker.Devices.DeviceTest do
       assert {:ok, all_devices} = Device.list_all(registry)
 
       devices
-      |> Enum.map(fn {name, _} -> %{name: name} end)
-      |> assert_lists_equal(all_devices, fn left, right ->
-        assert_maps_equal(left, right, [:name])
+      |> Enum.map(fn {name, [measurement]} ->
+        %{
+          name: name,
+          measurements: %{String.to_atom(measurement) => %{measurements: []}},
+          power_status: :on
+        }
       end)
+      |> assert_lists_equal(all_devices, &assert_maps_equal(&1, &2, Map.keys(&2)))
     end
   end
 
   describe "update/2" do
     test "updates settings for the given device", %{registry: registry} do
-      name = "lightbulb8"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
       assert {:ok, _} = Device.add_device(name, [measurement], S3, registry)
 
       assert {:ok,
@@ -123,23 +125,30 @@ defmodule DeviceTracker.Devices.DeviceTest do
                 max_measurements: 10,
                 group_name: "Living room"
               }} =
-               Device.update(name, %{
-                 :power_status => :off,
-                 :max_measurements => 10,
-                 "group_name" => "Living room"
-               }, registry)
+               Device.update(
+                 name,
+                 %{
+                   :power_status => :off,
+                   :max_measurements => 10,
+                   "group_name" => "Living room"
+                 },
+                 registry
+               )
     end
   end
 
   describe "delete/1" do
     test "deletes the given device", %{registry: registry} do
-      name = "lightbulb9"
-      measurement = "power_used"
+      name = random_string()
+      measurement = random_string()
 
       assert {:ok, _} = Device.add_device(name, [measurement], S3, registry)
-      assert {:ok, %{}} = Device.get(name, registry)
       assert {:ok, _} = Device.delete(name, registry)
       assert {:error, :not_found} = Device.get(name, registry)
     end
+  end
+
+  defp random_string() do
+    :crypto.strong_rand_bytes(64) |> Base.url_encode64() |> binary_part(0, 64)
   end
 end
