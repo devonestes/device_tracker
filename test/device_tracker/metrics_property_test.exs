021c509c1897c7d289f9f7d1b8fcd04985e6ae77
Devon Estes <devon.c.estes@gmail.com>
2020-09-26 14:20:53 +0200

    without property based tests

--- test/device_tracker/metrics_property_test.exs
+++ test/device_tracker/metrics_property_test.exs
@@ -5,42 +5,11 @@ defmodule DeviceTracker.MetricsPropertyTest do
   alias DeviceTracker.Metrics
 
   describe "average/1" do
-    property "never returns an error" do
-      forall nums <- non_empty(list(number())) do
-        {min, max} = Enum.min_max(nums)
-
-        avg = Metrics.average(nums)
-
-        assert min <= avg
-        assert max >= avg
-      end
-    end
   end
 
   describe "median/1" do
-    property "never returns an error" do
-      forall nums <- non_empty(list(number())) do
-        {min, max} = Enum.min_max(nums)
-
-        median = Metrics.median(nums)
-
-        assert min <= median
-        assert max >= median
-      end
-    end
   end
 
   describe "mean/1" do
-    property "never returns an error" do
-      forall nums <- non_empty(list(number())) do
-        mean =
-          nums
-          |> Enum.frequencies()
-          |> Enum.max_by(&elem(&1, 1))
-          |> elem(0)
-
-        assert mean == Metrics.mean(nums)
-      end
-    end
   end
 end
