4ee4c1655661f1e9a9c77c29cf9680c7287c04f1
Devon Estes <devon.c.estes@gmail.com>
2020-09-26 14:55:15 +0200

    removed api tests

--- test/device_tracker_web/features/api/devices_test.exs
+++ test/device_tracker_web/features/api/devices_test.exs
@@ -1,48 +1,21 @@
 defmodule DeviceTrackerWeb.Features.Api.DevicesTest do
   use DeviceTrackerWeb.FeatureCase, async: true
 
-  alias DeviceTracker.Devices.Device
-
   describe "POST /api/devices" do
+    @tag :skip
     test "creates a new device" do
-      name = random_string()
-      measurement = random_string()
-      params = %{name: name, measurements: [measurement]}
-      path = device_path(:create)
-      assert {:ok, %{status_code: 200, body: body}} = request(:post, path, Jason.encode!(params))
-      assert {:ok, ^params} = Jason.decode(body, keys: :atoms)
-      assert {:ok, %{name: ^name}} = Device.get(name)
     end
   end
 
   describe "PUT /devices/:id" do
+    @tag :skip
     test "updates the configuration for a device" do
-      name =  "device2"
-      measurements = ["power_usage"]
-      Device.add_device(name, measurements)
-
-      params = %{power_status: "off"}
-      path = device_path(:update, name)
-      assert {:ok, %{status_code: 200, body: body}} = request(:put, path, Jason.encode!(params))
-      assert {:ok, %{power_status: "off"}} = Jason.decode(body, keys: :atoms)
-      assert {:ok, %{power_status: "off"}} = Device.get(name)
     end
   end
 
   describe "DELETE /devices/:id" do
+    @tag :skip
     test "deletes a device" do
-      name =  "device3"
-      measurements = ["power_usage"]
-      Device.add_device(name, measurements)
-
-      path = device_path(:delete, name)
-      assert {:ok, %{status_code: 200, body: body}} = request(:delete, path)
-      assert %{} = Jason.decode!(body)
-      assert {:error, :not_found} = Device.get(name)
     end
-  end
-
-  defp random_string() do
-    :crypto.strong_rand_bytes(64) |> Base.url_encode64() |> binary_part(0, 64)
   end
 end
